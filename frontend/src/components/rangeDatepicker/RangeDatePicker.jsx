042fa32fa72b5393280133c570302061c183246d
A-ForssmanP <130503088+A-ForssmanP@users.noreply.github.com>
2024-06-02 21:58:40 +0200

    DatePicker-Changing package
    DatePicker-Changing package

--- frontend/src/components/rangeDatepicker/RangeDatePicker.jsx
+++ frontend/src/components/rangeDatepicker/RangeDatePicker.jsx
@@ -3,45 +3,41 @@ import "react-datepicker/dist/react-datepicker.css";
 import { useState } from "react";
 
 function RangeDatePicker() {
-  const [startDate, setStartDate] = useState(new Date());
-  const [endDate, setEndDate] = useState(null);
-
-  //handle dateChange
-  const onChange = (dates) => {
-    console.log(dates);
-    const [start, end] = dates;
-    setStartDate(start);
-    setEndDate(end);
-  };
-
-  const isWeekday = (date) => {
-    const day = date.getDay();
-    return day !== 0 && day !== 6;
-  };
-
-  const handleDateSelect = () => {
-    console.log("hej");
-  };
-
-  return (
-    <DatePicker
-      onSelect={handleDateSelect}
-      highlightDates={[new Date(), new Date()]}
-      // swapRange
-      // selected={startDate}
-      onChange={onChange}
-      startDate={startDate}
-      endDate={endDate}
-      selectsRange
-      // selectsDisabledDaysInRange
-      inline
-      calendarStartDay={1}
-      showWeekNumbers
-      filterDate={isWeekday}
-      // excludeDates={[addDays(new Date(), 1), addDays(new Date(), 5)]}
-      excludeDates={["2024-06-15"]}
-    />
-  );
+  // const [startDate, setStartDate] = useState(new Date());
+  // const [endDate, setEndDate] = useState(null);
+  // //handle dateChange
+  // const onChange = (dates) => {
+  //   console.log(dates);
+  //   const [start, end] = dates;
+  //   setStartDate(start);
+  //   setEndDate(end);
+  // };
+  // const isWeekday = (date) => {
+  //   const day = date.getDay();
+  //   return day !== 0 && day !== 6;
+  // };
+  // const handleDateSelect = () => {
+  //   console.log("hej");
+  // };
+  // return (
+  //   <DatePicker
+  //     onSelect={handleDateSelect}
+  //     highlightDates={[new Date(), new Date()]}
+  //     // swapRange
+  //     // selected={startDate}
+  //     onChange={onChange}
+  //     startDate={startDate}
+  //     endDate={endDate}
+  //     selectsRange
+  //     // selectsDisabledDaysInRange
+  //     inline
+  //     calendarStartDay={1}
+  //     showWeekNumbers
+  //     filterDate={isWeekday}
+  //     // excludeDates={[addDays(new Date(), 1), addDays(new Date(), 5)]}
+  //     excludeDates={["2024-06-15"]}
+  //   />
+  // );
 }
 
 export default RangeDatePicker;
