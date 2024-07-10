5813a5bee2e616188e28d1bbfb8359c5b56635fa
A-ForssmanP <130503088+A-ForssmanP@users.noreply.github.com>
2024-07-10 23:49:35 +0200

    add content and basic style

--- frontend/src/pages/homePage/HomePage.jsx
+++ frontend/src/pages/homePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import { Box, Card, CardContent, Typography } from "@mui/material";
+import { Box, Card, CardContent, Typography, Paper, Grid } from "@mui/material";
 import { orange } from "@mui/material/colors";
 
 /* content:
@@ -13,18 +13,56 @@ Klass lista med färäldrar
 
 function HomePage() {
   return (
-    <Box>
-      <Box
-        minHeight={200}
-        bgcolor={"rgba(230, 76, 255)"}
-        color={"rgb(255,255,255)"}
-        maxWidth={600}
+    <Grid container border={"4px solid green"} height={"100%"}>
+      <Grid
+        item
+        xs={10}
+        bgcolor={"yellow"}
+        display={"Flex"}
+        flexDirection={"column"}
+        height={"100%"}
       >
-        <Typography variant="h1" fontSize={22} pt={3}>
-          Välkommen,NAMN
-        </Typography>
-      </Box>
-    </Box>
+        <Box border={"1px solid red"}>
+          <Box
+          // minHeight={200}
+          // bgcolor={"rgba(230, 76, 255)"}
+          // color={"rgb(255,255,255)"}
+          // maxWidth={600}
+          >
+            <Typography variant="p">Hej,NAMN,</Typography>
+            <Typography variant="h2">Välkommen tillbaka!</Typography>
+          </Box>
+          <Box>
+            <Typography>Dagens datum:</Typography>
+            <Typography>1 Jan</Typography>
+          </Box>
+        </Box>
+        <Box flexGrow={1} border={"1px solid blue"}>
+          <Grid
+            container
+            justifyContent="center"
+            alignItems="center"
+            height={"100%"}
+          >
+            <Grid item xs={6}>
+              <Paper>X</Paper>
+            </Grid>
+            <Grid item xs={6}>
+              <Paper>X</Paper>
+            </Grid>
+            <Grid item xs={6}>
+              <Paper>X</Paper>
+            </Grid>
+            <Grid item xs={6}>
+              <Paper>X</Paper>
+            </Grid>
+          </Grid>
+        </Box>
+      </Grid>
+      <Grid item xs={2}>
+        s
+      </Grid>
+    </Grid>
   );
 }
 
