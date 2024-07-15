e4bb60d5ab0ddd1e030702e682e9c6799dbdcf87
A-ForssmanP <130503088+A-ForssmanP@users.noreply.github.com>
2024-07-15 23:48:24 +0200

    get the progress of school year, for the chart

--- frontend/src/components/chartYearProg/ChartYearProg.jsx
+++ frontend/src/components/chartYearProg/ChartYearProg.jsx
@@ -1,5 +1,6 @@
 import { Box, Typography } from "@mui/material";
-import { getDayOfYear } from "date-fns";
+// import { getDayOfYear } from "date-fns";
+import { eachDayOfInterval } from "date-fns";
 import DoughnutChart from "../doughnutChart/DoughnutChart";
 import { useState, useEffect } from "react";
 
@@ -11,30 +12,70 @@ function ChartYearProg() {
   });
 
   useEffect(() => {
-    const dayValues = handleTimeElapsed();
-    console.log(dayValues);
+    schoolYearProgress();
   }, []);
+
+  // get progress of school year and summer break
+  const schoolYearProgress = () => {
+    //get the intervall from start to end of days in school period
+    const schoolDaysInterval = eachDayOfInterval({
+      start: new Date("2024,7,14"),
+      end: new Date("2024,7,18"),
+    });
+    //check if current day is a school day
+    const currentDay = new Date();
+    const isSchoolDay = schoolDaysInterval.some(
+      (date) => date.toDateString() === currentDay.toDateString()
+    );
+
+    if (isSchoolDay) {
+      progressSchool(currentDay, schoolDaysInterval);
+    } else {
+      // progressSummerBreak()
+    }
+  };
+
+  // get progress of school period
+  const progressSchool = (currDay, intervall) => {
+    const intervallToString = intervall.map((day) => day.toDateString());
+    const dayToString = currDay.toDateString();
+
+    //get progress of days done
+    const dayInIntervall = intervallToString.indexOf(dayToString) + 1;
+
+    //get days remaining
+    const daysRemaining = intervall.length - dayInIntervall;
+
+    setChartData((currData) => {
+      return {
+        ...currData,
+        data: [daysRemaining, dayInIntervall],
+      };
+    });
+    console.log(dayInIntervall);
+    console.log(daysRemaining);
+  };
 
   /* CONTINUE HERE! 
     CALULATE FROM START DATE???
   */
   //get the day-number of the current day in the year
-  const getCurrentDayNumber = () => {
-    const result = getDayOfYear(new Date());
-    return result;
-  };
+  // const getCurrentDayNumber = () => {
+  //   const result = getDayOfYear(new Date());
+  //   return result;
+  // };
 
-  // caluclate the remaining days of schoolYear
-  const daysRemaining = (dayNumber) => {
-    return 178 - dayNumber;
-  };
+  // // caluclate the remaining days of schoolYear
+  // const daysRemaining = (dayNumber) => {
+  //   return 178 - dayNumber;
+  // };
 
-  // get days elapsed and days remaining of the schoolYear
-  const handleTimeElapsed = () => {
-    const isDayNumber = getCurrentDayNumber();
-    const isRemaining = daysRemaining(isDayNumber);
-    return [isRemaining, isDayNumber];
-  };
+  // // get days elapsed and days remaining of the schoolYear
+  // const handleTimeElapsed = () => {
+  //   const isDayNumber = getCurrentDayNumber();
+  //   const isRemaining = daysRemaining(isDayNumber);
+  //   return [isRemaining, isDayNumber];
+  // };
 
   return (
     <Box>
