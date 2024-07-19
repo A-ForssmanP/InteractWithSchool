20c0eab348950cbe89f8650bc73b2637c2d7053e
A-ForssmanP <130503088+A-ForssmanP@users.noreply.github.com>
2024-07-19 23:28:39 +0200

    add styles and functions for creating content, in notes component

--- frontend/src/components/dashBoardNotes/DashBoardNotes.jsx
+++ frontend/src/components/dashBoardNotes/DashBoardNotes.jsx
@@ -5,27 +5,44 @@ function DashBoardNotes() {
   const paperData = [
     {
       rows: 5,
-      rowStyle: { height: "3px", backgroundColor: "blue" },
+      rowStyle: { height: "3px", backgroundColor: "rgba(40,160,255)" },
       paperStyles: {
+        cursor: "pointer",
         zIndex: 100,
         backgroundColor: "lightblue",
         transform: `rotate(0deg)`,
+        borderRadius: "6px",
+        "&:hover": {
+          border: "2px solid rgba(40,160,255,0.4)",
+        },
       },
     },
-    // {
-    //   rotate: "10deg",
-    //   bgColor: "red",
-    //   rows: 5,
-    //   zIndex: 10,
-    //   paperStyles: {},
-    // },
-    // {
-    //   rotate: "-10deg",
-    //   bgColor: "yellow",
-    //   rows: 5,
-    //   zIndex: 1,
-    //   paperStyles: {},
-    // },
+    {
+      paperStyles: {
+        zIndex: 1,
+        backgroundColor: "rgba(40,160,255)",
+        transform: `rotate(-10deg)`,
+        borderRadius: "6px",
+        position: "absolute",
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+      },
+    },
+    {
+      paperStyles: {
+        zIndex: 0,
+        backgroundColor: "rgba(40,100,255)",
+        transform: `rotate(10deg)`,
+        borderRadius: "6px",
+        position: "absolute",
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+      },
+    },
   ];
 
   const createNotePaper = (indx, rows = 0, rowStyle, paperStyles) => {
@@ -44,21 +61,33 @@ function DashBoardNotes() {
         sx={paperStyles}
       >
         {innerRows.map((row, indx) => (
-          <Box key={indx} sx={rowStyle}></Box>
+          <Box key={indx} width={() => getRowWidth(indx)} sx={rowStyle}></Box>
         ))}
       </Box>
     );
   };
 
+  // get with of row
+  const getRowWidth = (index) => {
+    if (index === 0) {
+      return "50%";
+    } else if (index === 2) {
+      return "80%";
+    } else if (index === 4) {
+      return "90%";
+    } else return "100%";
+  };
+
   return (
-    <Box border={"3px solid black"} flex={1}>
+    <Box flex={1}>
       <Box
         position={"relative"}
-        border={"5px solid red"}
+        // border={"5px solid red"}
         height={"15rem"}
         width={"13rem"}
         margin={"0 auto"}
         display={"flex"}
+        borderRadius={"6px"}
       >
         {paperData.map((item, index) =>
           createNotePaper(index, item.rows, item.rowStyle, item.paperStyles)
