8ac18d0f74a98b30a05bf47f9036163c5b99ba82
A-ForssmanP <130503088+A-ForssmanP@users.noreply.github.com>
2024-07-23 22:33:45 +0200

    style the component

--- frontend/src/components/dashBoardNotesTextField/DashBoardNotesTextField.jsx
+++ frontend/src/components/dashBoardNotesTextField/DashBoardNotesTextField.jsx
@@ -1,31 +1,51 @@
 import { Card, TextField, Button, Box } from "@mui/material";
+import { useTheme } from "@emotion/react";
 
 function DashBoardNotesTextField({ handleClose }) {
+  const theme = useTheme();
+  console.log(theme);
   return (
     <Card
       sx={{
-        border: "1px solid red",
+        // border: "1px solid red",
         // opacity: 0.3,
         position: "absolute",
-        top: { xs: "-4.5em", sm: "-10.5em", md: "-12em" },
+        top: { xs: "-4.5em", sm: "-10.5em", md: "-11.5em" },
         // bottom: "-3.5em",
         left: { xs: "-0.9em", sm: "6em", md: "4em" },
         right: { xs: "-0.9em", sm: "-11em", md: "-13.3em", lg: "-16em" },
         zIndex: 100,
-        paddingTop: 1,
+        backgroundColor: theme.palette.grey[50],
       }}
     >
-      <TextField
-        multiline
-        name="notes-textField"
-        // defaultValue="Default Values"
-        label="Mina Anteckningar"
-        rows={12}
-        sx={{ border: "1px solid green", width: "100%" }}
-      />
-      <Box>
-        <Button>Spara</Button>
-        <Button onClick={handleClose}>Stäng</Button>
+      <Box component={"form"}>
+        <TextField
+          multiline
+          name="notes-textField"
+          // defaultValue="Default Values"
+          label="Mina Anteckningar"
+          id="Mina Anteckningar"
+          rows={12}
+          sx={{
+            width: "100%",
+            marginTop: 1,
+            // backgroundColor: "#ffe0b2",
+            // "&focus": "{ backgroundColor: red }",
+          }}
+          // inputProps={{ style: { "&:focus": "{ border: red }" } }}
+        />
+        <Box display={"flex"} gap={1} bgcolor={theme.palette.primary.light}>
+          <Button variant="contained" type="submit" sx={{ color: "white" }}>
+            Spara
+          </Button>
+          <Button
+            onClick={handleClose}
+            variant="contained"
+            sx={{ color: "white" }}
+          >
+            Stäng
+          </Button>
+        </Box>
       </Box>
     </Card>
   );
