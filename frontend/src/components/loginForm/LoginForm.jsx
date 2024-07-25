182ceef0fac46a6ff75123a0acb23fd236f48fcc
A-ForssmanP <130503088+A-ForssmanP@users.noreply.github.com>
2024-07-25 22:11:13 +0200

    add content to loginForm

--- frontend/src/components/loginForm/LoginForm.jsx
+++ frontend/src/components/loginForm/LoginForm.jsx
@@ -1,7 +1,25 @@
-import { Box, Paper } from "@mui/material";
+import { Box, Paper, Typography, Button, TextField } from "@mui/material";
 
 function LoginForm() {
-  return <Box>LoginForm</Box>;
+  return (
+    <Box>
+      <Paper>
+        <Typography>Logga in</Typography>
+        <Box component={"from"}>
+          <Button>Som demo användare</Button>
+          <Typography>Eller</Typography>
+        </Box>
+        <Box component={"form"}>
+          <Box>
+            <TextField />
+            <TextField />
+          </Box>
+          <Button>Logga in</Button>
+        </Box>
+        <Button>Skapa användare</Button>
+      </Paper>
+    </Box>
+  );
 }
 
 export default LoginForm;
