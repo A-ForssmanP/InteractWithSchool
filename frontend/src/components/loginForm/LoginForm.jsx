675a9a9a72b45de688e1ecc0bd5bc7d8edb96781
A-ForssmanP <130503088+A-ForssmanP@users.noreply.github.com>
2024-07-25 22:36:53 +0200

    handle loginform content

--- frontend/src/components/loginForm/LoginForm.jsx
+++ frontend/src/components/loginForm/LoginForm.jsx
@@ -5,14 +5,18 @@ function LoginForm() {
     <Box>
       <Paper>
         <Typography>Logga in</Typography>
-        <Box component={"from"}>
-          <Button>Som demo användare</Button>
-          <Typography>Eller</Typography>
+        <Box>
+          <Typography variant="h4" fontSize={14}>
+            Som demo användare:
+          </Typography>
+          <Button>Användare #1</Button>
+          <Button>Användare #2</Button>
         </Box>
+        <Typography>Eller</Typography>
         <Box component={"form"}>
           <Box>
-            <TextField />
-            <TextField />
+            <TextField id="username-input" label="Användarnamn" type="text" />
+            <TextField id="password-input" label="Lösenord" type="password" />
           </Box>
           <Button>Logga in</Button>
         </Box>
