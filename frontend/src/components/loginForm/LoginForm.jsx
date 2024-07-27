7449f49288e0b8485b555ac67df2c4e7720f449a
A-ForssmanP <130503088+A-ForssmanP@users.noreply.github.com>
2024-07-27 23:14:47 +0200

    layout and style to Login and LoginForm

--- frontend/src/components/loginForm/LoginForm.jsx
+++ frontend/src/components/loginForm/LoginForm.jsx
@@ -1,20 +1,30 @@
 import { Box, Paper, Typography, Button, TextField } from "@mui/material";
 import PersonIcon from "@mui/icons-material/Person";
+import { useTheme } from "@emotion/react";
 
 function LoginForm() {
+  const theme = useTheme();
   return (
-    <Box>
+    <Box display={"flex"} justifyContent={"center"} width={"100%"}>
       <Paper
+        elevation={3}
         sx={{
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
-          width: { sm: "fit-content" },
-          p: { xs: 1, sm: 2 },
+          width: "100%",
+          maxWidth: "34rem",
+          background: "rgba(255,255,255,.2)",
+          borderRadius: 5,
+          p: { xs: 1, sm: 4 },
         }}
       >
         <Typography fontSize={20}>Logga in</Typography>
-        <Box marginTop={1} marginBottom={1}>
+        <Box
+          marginTop={1}
+          marginBottom={1.4}
+          borderBottom={"1px solid lightgrey"}
+        >
           <Typography textAlign={"center"} variant="h4" fontSize={15}>
             Demo användare:
           </Typography>
@@ -22,20 +32,46 @@ function LoginForm() {
           <Button startIcon={<PersonIcon />}>Användare #2</Button>
         </Box>
         <Typography>Eller</Typography>
-        <Box component={"form"} mt={1} mb={8} width={"100%"} maxWidth={"20rem"}>
-          <Box display={"flex"} flexDirection={"column"} gap={2} mb={1}>
-            <TextField id="username-input" label="Användarnamn" type="text" />
-            <TextField id="password-input" label="Lösenord" type="password" />
+        <Box
+          component={"form"}
+          mt={1.4}
+          mb={15}
+          width={"100%"}
+          maxWidth={"20rem"}
+        >
+          <Box display={"flex"} flexDirection={"column"} gap={2} mb={2}>
+            <TextField
+              id="username-input"
+              label="Användarnamn"
+              type="text"
+              InputProps={{
+                style: {
+                  borderRadius: "1.6rem",
+                },
+              }}
+            />
+            <TextField
+              id="password-input"
+              label="Lösenord"
+              type="password"
+              InputProps={{
+                style: {
+                  borderRadius: "1.6rem",
+                },
+              }}
+            />
           </Box>
           <Box display={"flex"} justifyContent={"center"}>
             <Button
               variant="contained"
+              type="submit"
               sx={{
                 borderRadius: 4,
                 pt: 1,
                 pb: 1,
                 pl: 3,
                 pr: 3,
+                background: theme.palette.secondary.main,
               }}
             >
               Logga in
--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,9 +1,26 @@
+import { useTheme } from "@emotion/react";
 import LoginForm from "../../components/loginForm/LoginForm";
 import { Box } from "@mui/material";
 
 function Login() {
+  const theme = useTheme();
   return (
-    <Box>
+    <Box
+      display={"flex"}
+      flexDirection={"column"}
+      justifyContent={"center"}
+      height={"100%"}
+      sx={{
+        background: {
+          xs: `linear-gradient(to top left,${theme.palette.primary.main} 0%, ${theme.palette.primary.main} 
+         23% ,white 0%, white 100%)`,
+          md: `linear-gradient(to top left,${theme.palette.primary.main} 0%, ${theme.palette.primary.main} 
+         28% ,white 0%, white 100%)`,
+          lg: `linear-gradient(to top left,${theme.palette.primary.main} 0%, ${theme.palette.primary.main} 
+         30% ,white 0%, white 100%)`,
+        },
+      }}
+    >
       <LoginForm />
     </Box>
   );
