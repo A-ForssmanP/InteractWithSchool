74b5cb0327221188ddcd0028f079f8bfc63b3453
A-ForssmanP <130503088+A-ForssmanP@users.noreply.github.com>
2024-07-31 12:42:07 +0200

    middleware handling authentication. Adding comment with the stepts to make

---
+++ backend/middleware/cookieJwtAuth.js
@@ -0,0 +1,13 @@
+function cookieJwtAuth(req,res,next) {
+    try{
+        //get token cookie out of req
+        //chech the validation of token
+        //set userId from token to req.user property
+        //next()
+    } catch(err) {
+        //delete unvalid cookie 
+        //res with 404
+    }
+}
+
+module.exports = cookieJwtAuth
