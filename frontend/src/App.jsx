e2ad8dbd2c9fb2cd953c54e820f19502e8708f8a
A-ForssmanP <130503088+A-ForssmanP@users.noreply.github.com>
2024-08-09 09:17:47 +0200

    wrapp app with NewInboxCount context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,26 @@
 import Stack from "@mui/material/Stack";
-import Menu from "./components/menu/Menu";
-import PagesWrapper from "./components/pagesWrapper/PagesWrapper";
 import theme from "..//themeCustom";
 import { ThemeProvider } from "@emotion/react";
+import { newInboxCount } from "./context";
+import Menu from "./components/menu/Menu";
+import PagesWrapper from "./components/pagesWrapper/PagesWrapper";
+import { useState } from "react";
 
 function App() {
+  const [newInboxMCount, setNewInboxMCount] = useState(0);
   return (
-    <ThemeProvider theme={theme}>
-      <Stack
-        direction={{ xs: "column", md: "row" }}
-        height={{ sm: "100dvh" }}
-        sx={{ position: "relative" }}
-      >
-        <Menu />
-        <PagesWrapper />
-      </Stack>
-    </ThemeProvider>
+    <newInboxCount.Provider value={{ newInboxCount, setNewInboxMCount }}>
+      <ThemeProvider theme={theme}>
+        <Stack
+          direction={{ xs: "column", md: "row" }}
+          height={{ sm: "100dvh" }}
+          sx={{ position: "relative" }}
+        >
+          <Menu />
+          <PagesWrapper />
+        </Stack>
+      </ThemeProvider>
+    </newInboxCount.Provider>
   );
 }
 
