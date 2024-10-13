39599c89dee98f0455fd977e0e8ff958c4cd21aa
A-ForssmanP <130503088+A-ForssmanP@users.noreply.github.com>
2024-10-13 13:04:49 +0200

    cleanup code

--- frontend/src/pages/chat/ChatContacts.jsx
+++ frontend/src/pages/chat/ChatContacts.jsx
@@ -19,18 +19,10 @@ function ChatContacts() {
       display="flex"
       flexDirection="column"
     >
-      {/* <Box
-        sx={{
-          padding: { sm: 2 },
-          display: "flex",
-          justifyContent: "center",
-        }}
-      > */}
       <ChatContactsSearch
         chatList={contacts.chats}
         userId={contacts?.userData?._id}
       />
-      {/* </Box> */}
       <div
         style={{
           flex: "1",
