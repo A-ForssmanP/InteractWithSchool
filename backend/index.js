3c7a2ef8c3c34adcfbdd2f4b1cfdcafd7d4e114e
A-ForssmanP <130503088+A-ForssmanP@users.noreply.github.com>
2024-10-28 13:51:39 +0100

    new deploy

--- backend/index.js
+++ backend/index.js
@@ -62,7 +62,7 @@ if(process.env.NODE_ENV !== "production") {
 }
 
 //socket server
-server.listen(socketPort)
+server.listen(port)
 
 io.on('connection', (socket) => {
   console.log('a user connected');
